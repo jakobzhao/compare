6251d6c499ddee7b6308c3b63f7d1bf8a4f64bb5
jakobzhao <jakobzhao@gmail.com>
2024-08-23 09:07:09 +0800

    popup functions

--- js/map.js
+++ js/map.js
@@ -93,6 +93,62 @@ leftMap.on("load", () => {
 
   // organize layer z-index and which ones go on top of each other
   // leftMap.moveLayer("co_line_layer", "outage_heatmap");
+
+  // When a click event occurs on a feature in the places layer, open a popup at the
+  // location of the feature, with description HTML from its properties.
+  leftMap.on("click", "pfos_pnt_layer", (e) => {
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const description = e.features[0].properties.PC_NAME;
+
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    new maplibregl.Popup()
+      .setLngLat(coordinates)
+      .setHTML(description)
+      .addTo(leftMap);
+  });
+
+  // Change the cursor to a pointer when the mouse is over the places layer.
+  leftMap.on("mouseenter", "pfos_pnt_layer", () => {
+    leftMap.getCanvas().style.cursor = "pointer";
+  });
+
+  // Change it back to a pointer when it leaves.
+  leftMap.on("mouseleave", "pfos_pnt_layer", () => {
+    leftMap.getCanvas().style.cursor = "";
+  });
+
+  leftMap.on("click", "pfoa_pnt_layer", (e) => {
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const description = e.features[0].properties.PC_NAME;
+
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    new maplibregl.Popup()
+      .setLngLat(coordinates)
+      .setHTML(description)
+      .addTo(leftMap);
+  });
+
+  // Change the cursor to a pointer when the mouse is over the places layer.
+  leftMap.on("mouseenter", "pfoa_pnt_layer", () => {
+    leftMap.getCanvas().style.cursor = "pointer";
+  });
+
+  // Change it back to a pointer when it leaves.
+  leftMap.on("mouseleave", "pfoa_pnt_layer", () => {
+    leftMap.getCanvas().style.cursor = "";
+  });
 });
 
 // map containing equity matrix and all other data
